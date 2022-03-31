3f917f95ed375f74d64c56a8912cb851d8534246
spwpun <1736090121@qq.com>
2022-03-31 16:10:38 +0800

    commit in wsl terminal, add dynamic plan for regex string

---
+++ 10.正则表达式匹配.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode.cn id=10 lang=python3
+#
+# [10] 正则表达式匹配
+#
+
+# @lc code=start
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+
+        def matches(i: int, j: int) -> bool:
+            if i == 0:
+                return False
+            if p[j - 1] == '.':
+                return True
+            return s[i - 1] == p[j - 1]
+
+        f = [[False] * (n + 1) for _ in range(m + 1)]
+        f[0][0] = True
+        # 更新这个状态矩阵，f[i][j]代表s的前i个字符是否匹配p的前j个字符的状态，有状态转移方程
+        for i in range(m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == '*':
+                    f[i][j] |= f[i][j - 2]
+                    if matches(i, j - 1):
+                        f[i][j] |= f[i - 1][j]
+                else:
+                    if matches(i, j):
+                        f[i][j] |= f[i - 1][j - 1]
+        # print(f[m][n])
+        return f[m][n]
+
+# local test
+# s = Solution()
+# Solution.isMatch(s, "ccdsaqcqvqdwqdqdqaab", "cc.*c*b")
+
+# @lc code=end
+
