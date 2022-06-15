1fdc7c4a177af96fecf554f19009e10c95e8925a
spwpun <1736090121@qq.com>
2022-06-15 23:37:33 +0800

    add leetcode 45.跳跃游戏，tag: dp,贪心

--- 45.跳跃游戏-ii.py
+++ 45.跳跃游戏-ii.py
@@ -7,6 +7,17 @@
 # @lc code=start
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        
+        if len(nums) == 1: return 0
+        ans = 0
+        curDistance = 0
+        nextDistance = 0
+        for i in range(len(nums)):
+            nextDistance = max(i + nums[i], nextDistance)
+            if i == curDistance:
+                if curDistance != len(nums) - 1:
+                    ans += 1
+                    curDistance = nextDistance
+                    if nextDistance >= len(nums) - 1: break
+        return ans
 # @lc code=end
 
