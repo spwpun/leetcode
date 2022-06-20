fb00c022857908007cb3d19ed50d0689936125c8
spwpun <1736090121@qq.com>
2022-06-21 01:08:35 +0800

    add HJ52 edit distance

---
+++ HJ52_编辑距离.py
@@ -0,0 +1,23 @@
+def c_distance(str1, str2):
+    x = len(str1) + 1
+    y = len(str2) + 1
+    dp = [[0 for i in range(y)] for j in range(x)]
+#     print(dp)
+    for i in range(x):
+        dp[i][0] = i
+    for j in range(y):
+        dp[0][j] = j
+    for i in range(1, x):
+        for j in range(1, y):
+            if str1[i - 1] != str2[j - 1]:
+                dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1)
+            else:
+                dp[i][j] = dp[i - 1][j - 1]
+    
+#     print(dp)
+    return dp[i][j]
+
+str1 = input()
+str2 = input()
+lev_distance = c_distance(str1, str2)
+print(lev_distance)
